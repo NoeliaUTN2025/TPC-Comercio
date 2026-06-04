7f20897a5f16e8b656a035223eb51b26edb8044b
Dova0205 <joaqui.leon@hotmail.es>
2026-06-04 20:49:13 -0300

    Cambio de tipo de variable dni en cliente e implementacion de ClienteNegocio

--- TPC-Comercio/Dominio/Cliente.cs
+++ TPC-Comercio/Dominio/Cliente.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +9,7 @@ namespace Dominio
     public class Cliente
     {
         public int ID { get; set; }
-        public int DNI { get; set; } 
-        
+        public string DNI { get; set; } 
         public string Nombre { get; set; }
         public string Apellido { get; set; }
        
