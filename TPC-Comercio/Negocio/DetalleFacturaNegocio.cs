d7cdffe157ba79f6e8585a9412c0815c09e68614
Dova0205 <joaqui.leon@hotmail.es>
2026-06-30 17:13:03 -0300

    ajustes para devolver id de factura y los detalles

--- TPC-Comercio/Negocio/DetalleFacturaNegocio.cs
+++ TPC-Comercio/Negocio/DetalleFacturaNegocio.cs
@@ -11,7 +11,6 @@ namespace Negocio
             AccesoDatos.AccesoDatos datos = new AccesoDatos.AccesoDatos();
             try
             {
-                // Usamos consulta directa por pragmatismo (evitamos tocar SQL con nuevos SPs)
                 datos.setearConsulta("INSERT INTO DetalleFacturas (IdFactura, IdProducto, Cantidad, PrecioCompra, PorcentajeGanancia, PrecioVenta, IdLote) VALUES (@IdFactura, @IdProducto, @Cantidad, @PrecioCompra, @PorcentajeGanancia, @PrecioVenta, @IdLote); SELECT SCOPE_IDENTITY();");
                 datos.setearParametro("@IdFactura", detalle.Factura.Id);
                 datos.setearParametro("@IdProducto", detalle.Producto.Id);
@@ -27,6 +26,44 @@ namespace Negocio
                     return Convert.ToInt32(datos.Lector[0]);
                 }
                 return 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public System.Collections.Generic.List<DetalleFactura> ListarPorFactura(int idFactura)
+        {
+            System.Collections.Generic.List<DetalleFactura> lista = new System.Collections.Generic.List<DetalleFactura>();
+            AccesoDatos.AccesoDatos datos = new AccesoDatos.AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT DF.Id, DF.IdProducto, P.NombreProducto, DF.Cantidad, DF.PrecioCompra, DF.PorcentajeGanancia, DF.PrecioVenta, DF.Subtotal, DF.IdLote FROM DetalleFacturas DF INNER JOIN Productos P ON DF.IdProducto = P.Id WHERE DF.IdFactura = @IdFactura");
+                datos.setearParametro("@IdFactura", idFactura);
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    DetalleFactura aux = new DetalleFactura();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Cantidad = (int)datos.Lector["Cantidad"];
+                    aux.PrecioCompra = (decimal)datos.Lector["PrecioCompra"];
+                    aux.PorcentajeGanancia = (decimal)datos.Lector["PorcentajeGanancia"];
+                    aux.PrecioVenta = (decimal)datos.Lector["PrecioVenta"];
+                    aux.Subtotal = (decimal)datos.Lector["Subtotal"];
+                    aux.IdLote = datos.Lector["IdLote"] != DBNull.Value ? (int?)datos.Lector["IdLote"] : null;
+                    
+                    aux.Producto = new Producto();
+                    aux.Producto.Id = (int)datos.Lector["IdProducto"];
+                    aux.Producto.NombreProducto = (string)datos.Lector["NombreProducto"];
+                    
+                    lista.Add(aux);
+                }
+                return lista;
             }
             catch (Exception ex)
             {
