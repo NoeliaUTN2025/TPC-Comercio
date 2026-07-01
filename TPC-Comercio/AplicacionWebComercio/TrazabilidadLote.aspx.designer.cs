edb08f8a0e101138dd23f5979f8ac05ccadea05a
Julian-Negrete <juliannegrete77@gmail.com>
2026-07-01 14:41:04 -0300

    vista de trazabilidad de lotes por producto

---
+++ TPC-Comercio/AplicacionWebComercio/TrazabilidadLote.aspx.designer.cs
@@ -0,0 +1,11 @@
+namespace AplicacionWebComercio
+{
+    public partial class TrazabilidadLote
+    {
+        protected global::System.Web.UI.WebControls.DropDownList ddlProducto;
+        protected global::System.Web.UI.WebControls.Panel        pnlResultados;
+        protected global::System.Web.UI.WebControls.Literal      litStockActual;
+        protected global::System.Web.UI.WebControls.GridView     dgvLotes;
+        protected global::System.Web.UI.WebControls.GridView     dgvVentas;
+    }
+}
