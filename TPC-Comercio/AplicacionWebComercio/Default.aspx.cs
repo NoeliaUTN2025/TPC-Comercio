5f0484a913a984fd5f85e13445ecd6709f63e198
Dova0205 <joaqui.leon@hotmail.es>
2026-07-01 17:02:44 -0300

    implementa tarjetas estadisticas para admin

--- TPC-Comercio/AplicacionWebComercio/Default.aspx.cs
+++ TPC-Comercio/AplicacionWebComercio/Default.aspx.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
+using Negocio;
+using Dominio;
 
 namespace AplicacionWebComercio
 {
@@ -11,7 +14,36 @@ namespace AplicacionWebComercio
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                Usuario u = Session["Usuario"] as Usuario;
+                if (Seguridad.SesionActiva(u) && (Seguridad.EsAdmin(u) || Seguridad.EsVendedor(u)))
+                {
+                    pnlDashboard.Visible = true;
+                    pnlMensajeAnonimo.Visible = false;
 
+                    try
+                    {
+                        DashboardNegocio negocio = new DashboardNegocio();
+                        DashboardStats stats = negocio.ObtenerEstadisticas();
+
+                        litTotalVentas.Text = "$" + stats.TotalVentasMes.ToString("0.00");
+                        litTotalCompras.Text = "$" + stats.TotalComprasMes.ToString("0.00");
+                        litBajoStock.Text = stats.ProductosBajoStock.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        litTotalVentas.Text = "Error";
+                        litTotalCompras.Text = "Error";
+                        litBajoStock.Text = "Error";
+                    }
+                }
+                else
+                {
+                    pnlDashboard.Visible = false;
+                    pnlMensajeAnonimo.Visible = true;
+                }
+            }
         }
     }
 }
--- TPC-Comercio/AplicacionWebComercio/Default.aspx.designer.cs
+++ TPC-Comercio/AplicacionWebComercio/Default.aspx.designer.cs
@@ -1,4 +1,4 @@
-﻿//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // <generado automáticamente>
 //     Este código fue generado por una herramienta.
 //
@@ -13,5 +13,50 @@ namespace AplicacionWebComercio
 
     public partial class _Default
     {
+
+        /// <summary>
+        /// Control pnlDashboard.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlDashboard;
+
+        /// <summary>
+        /// Control litTotalVentas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litTotalVentas;
+
+        /// <summary>
+        /// Control litTotalCompras.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litTotalCompras;
+
+        /// <summary>
+        /// Control litBajoStock.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litBajoStock;
+
+        /// <summary>
+        /// Control pnlMensajeAnonimo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlMensajeAnonimo;
     }
 }
